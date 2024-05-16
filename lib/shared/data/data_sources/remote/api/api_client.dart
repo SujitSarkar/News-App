b4f445c7c4893d884de34da6cec1d877f5364d05
Sujit Sarkar <snigdho532@gmail.com>
2024-05-17 01:56:49 +0600

    Api functions updated

--- lib/shared/data/data_sources/remote/api/api_client.dart
+++ lib/shared/data/data_sources/remote/api/api_client.dart
@@ -1,6 +1,6 @@
 part of 'remote_imports.dart';
 
-class ApiClient {
+class ApiClient extends CacheManager {
   late BaseOptions baseOptions;
   late Dio dio;
   Options options = Options();
@@ -10,45 +10,63 @@ class ApiClient {
     dio = Dio(baseOptions);
     options =
         Options(headers: {'Authorization': 'Bearer ${ApiConstant.apiKey}'});
-    // dio.interceptors.add(PrettyDioLogger());
   }
 
-  Future<Response> getRequest(
-      {required String path, bool isTokenRequired = false}) async {
-    try {
-      Response response = await dio.get(path, options: options);
-      debugPrint('\n\nURL: ${ApiConstant.baseUrl}$path');
-      debugPrint('STATUS CODE: ${response.statusCode}');
-      debugPrint(
-          'RESPONSE: ${response.data.toString().substring(0, 1000)}\n\n');
-      return response;
-    } on DioException catch (e) {
-      if (e.response != null) {
-        throw ApiException(message: e.response?.statusMessage);
-      } else {
-        throw ApiException(message: e.message);
+  Future<Response> getRequest({required String path}) async {
+    final online = await CennectivityService.instance.isOnline();
+    if (online == false) {
+      try {
+        return tryGetCachedResponse(path: path);
+      } catch (error) {
+        throw ApiException(message: error.toString());
       }
-    } catch (error) {
-      throw ApiException(message: error.toString());
+    } else {
+      try {
+        final Response response = await dio.get(path, options: options);
+        await cacheResponse(path: path, response: response);
+        logResponse(response);
+        return response;
+      } on DioException catch (e) {
+        if (e.response != null) {
+          throw ApiException(message: e.response?.statusMessage);
+        } else {
+          throw ApiException(message: e.message);
+        }
+      } catch (error) {
+        throw ApiException(message: error.toString());
+      }
     }
   }
 
   Future<Response> postRequest(
-      {required String path,
-      Map<String, dynamic>? body,
-      bool isTokenRequired = false}) async {
-    try {
-      Response response = await dio.post(path, data: body, options: options);
-      debugPrint('\n\nURL: ${ApiConstant.baseUrl}$path');
-      debugPrint('STATUS CODE: ${response.statusCode}');
-      debugPrint('RESPONSE: ${response.data.toString().substring(0, 500)}\n\n');
-      return response;
-    } on DioException catch (e) {
-      if (e.response != null) {
-        throw ApiException(message: e.response?.statusMessage);
-      } else {
-        throw ApiException(message: e.message);
+      {required String path, Map<String, dynamic>? body}) async {
+    final online = await CennectivityService.instance.isOnline();
+    if (online == false) {
+      try {
+        return tryGetCachedResponse(path: path);
+      } catch (error) {
+        throw ApiException(message: error.toString());
+      }
+    } else {
+      try {
+        Response response = await dio.post(path, data: body, options: options);
+        await cacheResponse(path: path, response: response);
+        logResponse(response);
+        return response;
+      } on DioException catch (e) {
+        if (e.response != null) {
+          throw ApiException(message: e.response?.statusMessage);
+        } else {
+          throw ApiException(message: e.message);
+        }
       }
     }
+  }
+
+  void logResponse(Response response) {
+    debugPrint('URL         : ${response.requestOptions.uri}');
+    debugPrint('STATUS CODE : ${response.statusCode}');
+    debugPrint(
+        'RESPONSE    : ${response.data.toString().substring(0, 500)}\n\n');
   }
 }
