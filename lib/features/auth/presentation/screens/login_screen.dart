023da8693b93e3eb8a0060e5e49d2352ca35b6db
Sujit Sarkar <snigdho532@gmail.com>
2024-05-17 18:59:39 +0600

    back button added to signin & signup screen

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -9,77 +9,75 @@ class LoginScreen extends StatelessWidget {
         init: LoginController(AuthService()),
         builder: (controller) {
           return Scaffold(
-            body: SafeArea(
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.all(16),
-                child: Form(
-                  key: controller.loginFormKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 16),
-                      Bouncy(
-                          duration: const Duration(milliseconds: 2000),
-                          lift: 20,
-                          ratio: 0.5,
-                          pause: 0.5,
-                          child: const Icon(
-                            FeatherIcons.unlock,
-                            size: 100,
-                            color: AppColors.primaryColor,
-                          )),
-                      const Text(
-                        'Login to Bookmark News',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: AppColors.primaryColor, fontSize: 24),
+            appBar: AppBar(),
+            body: SingleChildScrollView(
+              padding: const EdgeInsets.all(16),
+              child: Form(
+                key: controller.loginFormKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Bouncy(
+                        duration: const Duration(milliseconds: 2000),
+                        lift: 20,
+                        ratio: 0.5,
+                        pause: 0.5,
+                        child: const Icon(
+                          FeatherIcons.unlock,
+                          size: 100,
+                          color: AppColors.primaryColor,
+                        )),
+                    const Text(
+                      'Login to Bookmark News',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: AppColors.primaryColor, fontSize: 24),
+                    ),
+                    const SizedBox(height: 40),
+                    TextFormFieldWidget(
+                      controller: controller.emailController,
+                      hintText: "Email address",
+                      textInputType: TextInputType.emailAddress,
+                      required: true,
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormFieldWidget(
+                      controller: controller.passwordController,
+                      hintText: "Password",
+                      textInputType: TextInputType.visiblePassword,
+                      required: true,
+                      obscure: true,
+                    ),
+                    const SizedBox(height: 20),
+                    Obx(() => SolidButton(
+                        onTap: () {
+                          controller.userLogin(context);
+                        },
+                        isLoading: controller.isLoading.value,
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ))),
+                    RichText(
+                      text: TextSpan(
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        children: [
+                          const TextSpan(text: 'Don\'t have an account? '),
+                          TextSpan(
+                            text: 'Signup',
+                            style: const TextStyle(
+                                color: AppColors.secondaryColor,
+                                fontWeight: FontWeight.w700),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                pushTo(AppRouter.signup);
+                              },
+                          ),
+                        ],
                       ),
-                      const SizedBox(height: 40),
-                      TextFormFieldWidget(
-                        controller: controller.emailController,
-                        hintText: "Email address",
-                        textInputType: TextInputType.emailAddress,
-                        required: true,
-                      ),
-                      const SizedBox(height: 16),
-                      TextFormFieldWidget(
-                        controller: controller.passwordController,
-                        hintText: "Password",
-                        textInputType: TextInputType.visiblePassword,
-                        required: true,
-                        obscure: true,
-                      ),
-                      const SizedBox(height: 20),
-                      Obx(() => SolidButton(
-                          onTap: () {
-                            controller.userLogin(context);
-                          },
-                          isLoading: controller.isLoading.value,
-                          child: const Text(
-                            'Login',
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                          ))),
-                      RichText(
-                        text: TextSpan(
-                          style: Theme.of(context).textTheme.bodyMedium,
-                          children: [
-                            const TextSpan(text: 'Don\'t have an account? '),
-                            TextSpan(
-                              text: 'Signup',
-                              style: const TextStyle(
-                                  color: AppColors.secondaryColor,
-                                  fontWeight: FontWeight.w700),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  pushTo(AppRouter.signup);
-                                },
-                            ),
-                          ],
-                        ),
-                      ).paddingOnly(top: 16),
-                    ],
-                  ),
+                    ).paddingOnly(top: 16),
+                  ],
                 ),
               ),
             ),
--- lib/features/auth/presentation/screens/signup_screen.dart
+++ lib/features/auth/presentation/screens/signup_screen.dart
@@ -8,92 +8,89 @@ class Signupcreen extends StatelessWidget {
     return GetBuilder<SignupController>(
       init: SignupController(AuthService()),
       builder: (controller) => Scaffold(
-        body: SafeArea(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(16),
-            child: Form(
-              key: controller.signupFormKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 16),
-                  Bouncy(
-                      duration: const Duration(milliseconds: 2000),
-                      lift: 20,
-                      ratio: 0.5,
-                      pause: 0.5,
-                      child: const Icon(
-                        FeatherIcons.user,
-                        size: 100,
-                        color: AppColors.primaryColor,
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: controller.signupFormKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Bouncy(
+                    duration: const Duration(milliseconds: 2000),
+                    lift: 20,
+                    ratio: 0.5,
+                    pause: 0.5,
+                    child: const Icon(
+                      FeatherIcons.user,
+                      size: 100,
+                      color: AppColors.primaryColor,
+                    )),
+                const Text(
+                  "Create new account to start your journey",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: AppColors.primaryColor, fontSize: 24),
+                ),
+                const SizedBox(height: 40),
+                TextFormFieldWidget(
+                  controller: controller.nameController,
+                  hintText: "Full name",
+                  textCapitalization: TextCapitalization.words,
+                  required: true,
+                ),
+                const SizedBox(height: 16),
+                TextFormFieldWidget(
+                  controller: controller.emailController,
+                  hintText: "Email address",
+                  textInputType: TextInputType.emailAddress,
+                  required: true,
+                ),
+                const SizedBox(height: 16),
+                TextFormFieldWidget(
+                  controller: controller.passwordController,
+                  hintText: "Password",
+                  textInputType: TextInputType.visiblePassword,
+                  required: true,
+                  obscure: true,
+                ),
+                const SizedBox(height: 16),
+                TextFormFieldWidget(
+                  controller: controller.confirmPasswordController,
+                  hintText: "Confirm password",
+                  textInputType: TextInputType.visiblePassword,
+                  required: true,
+                  obscure: true,
+                ),
+                const SizedBox(height: 20),
+                Obx(
+                  () => SolidButton(
+                      onTap: () => controller.userSignup(context),
+                      isLoading: controller.isLoading.value,
+                      child: const Text(
+                        'Signup',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
                       )),
-                  const Text(
-                    "Create new account to start your journey",
-                    textAlign: TextAlign.center,
-                    style:
-                        TextStyle(color: AppColors.primaryColor, fontSize: 24),
+                ),
+                RichText(
+                  text: TextSpan(
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    children: [
+                      const TextSpan(text: 'Already have an account? '),
+                      TextSpan(
+                        text: 'Login',
+                        style: const TextStyle(
+                            color: AppColors.secondaryColor,
+                            fontWeight: FontWeight.w700),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            popScreen();
+                          },
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 40),
-                  TextFormFieldWidget(
-                    controller: controller.nameController,
-                    hintText: "Full name",
-                    textCapitalization: TextCapitalization.words,
-                    required: true,
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormFieldWidget(
-                    controller: controller.emailController,
-                    hintText: "Email address",
-                    textInputType: TextInputType.emailAddress,
-                    required: true,
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormFieldWidget(
-                    controller: controller.passwordController,
-                    hintText: "Password",
-                    textInputType: TextInputType.visiblePassword,
-                    required: true,
-                    obscure: true,
-                  ),
-                  const SizedBox(height: 16),
-                  TextFormFieldWidget(
-                    controller: controller.confirmPasswordController,
-                    hintText: "Confirm password",
-                    textInputType: TextInputType.visiblePassword,
-                    required: true,
-                    obscure: true,
-                  ),
-                  const SizedBox(height: 20),
-                  Obx(
-                    () => SolidButton(
-                        onTap: () => controller.userSignup(context),
-                        isLoading: controller.isLoading.value,
-                        child: const Text(
-                          'Signup',
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                        )),
-                  ),
-                  RichText(
-                    text: TextSpan(
-                      style: Theme.of(context).textTheme.bodyMedium,
-                      children: [
-                        const TextSpan(text: 'Already have an account? '),
-                        TextSpan(
-                          text: 'Login',
-                          style: const TextStyle(
-                              color: AppColors.secondaryColor,
-                              fontWeight: FontWeight.w700),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              popScreen();
-                            },
-                        ),
-                      ],
-                    ),
-                  ).paddingOnly(top: 16),
-                ],
-              ),
+                ).paddingOnly(top: 16),
+              ],
             ),
           ),
         ),
